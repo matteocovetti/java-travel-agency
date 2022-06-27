3d345d6b24107f136b3aec41b9a24c6103cf4529
matteocovetti <covetti.matteo@gmail.com>
2022-06-27 12:02:43 +0200

    first commit

---
+++ src/jana60/Agenzia.java
@@ -0,0 +1,21 @@
+package jana60;
+
+public class Agenzia {
+
+	public static void main(String[] args) {
+		/*
+		 * Aggiungere una classe Agenzia con metodo main, dove chiediamo all’operatore
+		 * se vuole inserire una nuova vacanza o uscire. Se vuole proseguire con
+		 * l’inserimento va chiesta la destinazione, il giorno, mese e anno di partenza
+		 * e il giorno, mese anno di ritorno. Con questi dati va generata una nuova
+		 * vacanza e in console verrà stampato un messaggio del tipo: “Hai prenotato una
+		 * vacanza di [durata della vacanza] a [destinazione] dal [data inizio
+		 * formattata] al [data fine formattata]“. Se la creazione della vacanza genera
+		 * un errore, il programma non deve interrompersi ma va gestito con dei messaggi
+		 * di errore che permettono all’utente di capire cosa è andato storto e di
+		 * ripetere l’inserimento.
+		 */
+
+	}
+
+}
---
+++ src/jana60/Vacanza.java
@@ -0,0 +1,13 @@
+package jana60;
+
+public class Vacanza {
+	/*
+	 * Nel progetto java-travel-agency, creare la classe Vacanza caratterizzata da:
+	 * - destinazione - data inizio - data fine Quando viene creata una nuova
+	 * Vacanza vanno effettuati dei controlli: - destinazione, data inizio e data
+	 * fine non possono essere null - la data inizio non può essere già passata - la
+	 * data fine non può essere prima della data inizio Se fallisce la validazione
+	 * vanno sollevate opportune eccezioni La classe Vacanza espone un metodo per
+	 * calcolare la durata della vacanza (in giorni, mesi, anni).
+	 */
+}
