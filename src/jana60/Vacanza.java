741be2d3d26bd177ac21218d6b31f30f7ea53e72
matteocovetti <covetti.matteo@gmail.com>
2022-06-27 12:44:46 +0200

    add Vacanza

--- src/jana60/Vacanza.java
+++ src/jana60/Vacanza.java
@@ -1,5 +1,8 @@
 package jana60;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Vacanza {
 	/*
 	 * Nel progetto java-travel-agency, creare la classe Vacanza caratterizzata da:
@@ -10,4 +13,80 @@ public class Vacanza {
 	 * vanno sollevate opportune eccezioni La classe Vacanza espone un metodo per
 	 * calcolare la durata della vacanza (in giorni, mesi, anni).
 	 */
+
+	// attributi
+	private DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	private String destinazione, dataInizio, dataFine;
+
+	private LocalDate dataInizioForm = LocalDate.parse(dataInizio, format);
+	private LocalDate dataFineForm = LocalDate.parse(dataFine, format);
+
+	// costruttore
+	public Vacanza(String destinazione, String dataInizio, String dataFine)
+			throws IllegalArgumentException, NullPointerException {
+		super();
+
+		validaDestinazione(destinazione);
+		validaDataInizio(dataInizio);
+		validaDataFine(dataFine);
+
+		this.destinazione = destinazione;
+		this.dataInizio = dataInizio;
+		this.dataFine = dataFine;
+	}
+
+	// getter e setter
+	public String getDestinazione() {
+		return destinazione;
+	}
+
+	public void setDestinazione(String destinazione) throws NullPointerException {
+		validaDestinazione(destinazione);
+		this.destinazione = destinazione;
+	}
+
+	public String getDataInizio() {
+		return dataInizio;
+	}
+
+	public void setDataInizio(String dataInizio) throws IllegalArgumentException, NullPointerException {
+		validaDataInizio(dataInizio);
+		this.dataInizio = dataInizio;
+	}
+
+	public String getDataFine() {
+		return dataFine;
+	}
+
+	public void setDataFine(String dataFine) throws IllegalArgumentException, NullPointerException {
+		validaDataFine(dataFine);
+		this.dataFine = dataFine;
+	}
+
+	// metodi di validazione
+	private void validaDataInizio(String dataInizioInput) throws IllegalArgumentException, NullPointerException {
+		LocalDate oggi = LocalDate.now();
+		if (dataInizioInput == null) {
+			throw new NullPointerException("Data inizio non può essere null");
+		}
+		if (LocalDate.parse(dataInizioInput, format).isBefore(oggi)) {
+			throw new IllegalArgumentException("La data di partenza non può essere precendente alla data odierna");
+		}
+	}
+
+	private void validaDataFine(String dataFineInput) throws IllegalArgumentException, NullPointerException {
+		if (dataFineInput == null) {
+			throw new NullPointerException("Data fine non può essere null");
+		}
+		if (LocalDate.parse(dataFineInput, format).isBefore(LocalDate.parse(dataInizio, format))) {
+			throw new IllegalArgumentException("La data di ritorno non può essere precedente alla data di partenza");
+		}
+	}
+
+	private void validaDestinazione(String destinazioneInput) {
+		if (destinazioneInput == null) {
+			throw new NullPointerException("La destinazione non può essere null");
+		}
+	}
+
 }
